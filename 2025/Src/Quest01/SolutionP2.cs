08442718f8026b3a8da2dde58809c774a9291725
Hunor Tot-Bagi <hunor.totbagi@rationaletech.com>
2025-11-10 20:17:28 +0100

    finish quest 1 P2

---
+++ 2025/Src/Quest01/SolutionP2.cs
@@ -0,0 +1,40 @@
+﻿namespace Src.Quest01;
+
+public class SolutionP2
+{
+    public static (string[] names, string[] directions) ReadNamesAndDirections(string testData)
+    {
+        var lines = File.ReadAllLines(testData);
+
+        return (lines[0].Split(","), lines[2].Split(","));
+    }
+
+    public static string Solve(string filePath) 
+    {
+        var (listOfNames, listOfMoves) = ReadNamesAndDirections(filePath);
+
+        var i = 0;
+        var distance = 0;
+
+        foreach (var move in listOfMoves)
+        {
+            distance = int.Parse(move.Substring(1));
+            if (move[0] == 'R')
+            {
+                i += distance;
+                if (i > listOfNames.Length - 1)
+                    i %= listOfNames.Length;
+            }
+            else
+            {
+                i -= distance;
+                if (i < 0)
+                    i = ((i % listOfNames.Length) + listOfNames.Length) % listOfNames.Length;
+            }
+
+
+        }
+
+        return listOfNames[i];
+    }
+}
--- 2025/Test/Quest01.cs
+++ 2025/Test/Quest01.cs
@@ -24,13 +24,26 @@ public class Quest01
     }
 
     [Fact]
-    public void SolveTest()
+    public void SolutionP1SolveTest()
     {
         // Arrange
         var expectedNames = "Fyrryn";
 
         // Act
         var result = SolutionP1.Solve(_testData);
+
+        // Assert
+        result.Should().Be(expectedNames);
+    }
+
+    [Fact]
+    public void SolutionP2SolveTest()
+    {
+        // Arrange
+        var expectedNames = "Elarzris";
+
+        // Act
+        var result = SolutionP2.Solve(_testData);
 
         // Assert
         result.Should().Be(expectedNames);
