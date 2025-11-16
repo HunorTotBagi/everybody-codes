d0f000d0a1b79e0612ac3f650784ff3650e4edf4
Hunor Tot-Bagi <hunor.totbagi@rationaletech.com>
2025-11-16 16:56:38 +0100

    finish quest4 p3

---
+++ 2025/Src/Quest04/SolutionP3.cs
@@ -0,0 +1,36 @@
+﻿ namespace Src.Quest04;
+
+public class SolutionP3
+{
+    public static double Calculate(string filePath, long turns)
+    {
+        var input = ReadNumber(filePath);
+        double result = 1;
+
+        foreach (var item in input)
+            result *= item[0] / item[1];
+
+        return Math.Floor(result * turns);
+    }
+
+    public static List<double[]> ReadNumber(string filePath) 
+    {
+        var lines = File.ReadAllLines(filePath);
+        var result = new List<double[]>();
+
+        var asd = lines[1].Split('|');
+        var first = new double[] { double.Parse(lines[0]) , double.Parse(asd[0]) };
+
+        result.Add(first);
+
+        for(var i=1; i<lines.Length - 1; i++)
+        {
+            var a = lines[i].Split('|');
+            var b = lines[i + 1].Split('|');
+
+            result.Add([double.Parse(a[1]), double.Parse(b[0])]);
+        }
+
+        return result;
+    }
+}
--- 2025/Test/Quest04Test.cs
+++ 2025/Test/Quest04Test.cs
@@ -8,6 +8,8 @@ public class Quest04Test
 {
     private readonly string _testData1 = AppDomain.CurrentDomain.BaseDirectory + "../../../../Src/Quest04/testData1.txt";
     private readonly string _testData2 = AppDomain.CurrentDomain.BaseDirectory + "../../../../Src/Quest04/testData2.txt";
+    private readonly string _testData3 = AppDomain.CurrentDomain.BaseDirectory + "../../../../Src/Quest04/testData3.txt";
+    private readonly string _testData4 = AppDomain.CurrentDomain.BaseDirectory + "../../../../Src/Quest04/testData4.txt";
 
     [Fact]
     public void ReadNumbersTest()
@@ -73,6 +75,73 @@ public class Quest04Test
 
         // Act
         var result = SolutionP2.Calculate(_testData2, turns);
+
+        // Assert
+        result.Should().Be(expected);
+    }
+
+    [Fact]
+    public void ReadNumbersTest2()
+    {
+        // Arrange
+        var expected = new List<double[]>
+        {
+            new double[] { 5, 5 },
+            new double[] { 10, 10 },
+            new double[] { 20, 5 }
+        };
+
+        // Act
+        var result = SolutionP3.ReadNumber(_testData3);
+
+        // Assert
+        result.Should().BeEquivalentTo(expected);
+    }
+
+    [Fact]
+    public void ReadNumbersTest3()
+    {
+        // Arrange
+        var expected = new List<double[]>
+        {
+            new double[] { 5, 7 },
+            new double[] { 21, 18 },
+            new double[] { 36, 27 },
+            new double[] { 27, 10 },
+            new double[] { 50, 10 },
+            new double[] { 50, 11 }
+        };
+
+        // Act
+        var result = SolutionP3.ReadNumber(_testData4);
+
+        // Assert
+        result.Should().BeEquivalentTo(expected);
+    }
+
+    [Fact]
+    public void CalculateTest5()
+    {
+        // Arrange
+        var turns = 100;
+        var expected = 400;
+
+        // Act
+        var result = SolutionP3.Calculate(_testData3, turns);
+
+        // Assert
+        result.Should().Be(expected);
+    }
+
+    [Fact]
+    public void CalculateTest6()
+    {
+        // Arrange
+        var turns = 100;
+        var expected = 6818;
+
+        // Act
+        var result = SolutionP3.Calculate(_testData4, turns);
 
         // Assert
         result.Should().Be(expected);
