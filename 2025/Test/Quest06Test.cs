3fbb18f21ecbb7d9592af636edd6c7e67cef9972
Hunor Tot-Bagi <hunor.totbagi@rationaletech.com>
2026-01-23 21:31:03 +0100

    feat: finish quest6 p1

---
+++ 2025/Test/Quest06Test.cs
@@ -0,0 +1,36 @@
+﻿using FluentAssertions;
+using Src.Quest06;
+using Xunit;
+
+namespace Test;
+
+public class Quest06Test
+{
+    private readonly string _testData1 = AppDomain.CurrentDomain.BaseDirectory + "../../../../Src/Quest06/testData1.txt";
+
+    [Fact]
+    public void ReadFile()
+    {
+        // Arrange
+        var expected = "ABabACacBCbca";
+
+        // Act
+        var result = SolutionP1.ReadFile(_testData1);
+
+        // Assert
+        result.Should().BeEquivalentTo(expected);
+    }
+
+    [Fact]
+    public void CountMentorsTest()
+    {
+        // Arrange
+        var expected = 5;
+
+        // Act
+        var result = SolutionP1.CountMentors(_testData1);
+
+        // Assert
+        result.Should().Be(expected);
+    }
+}
