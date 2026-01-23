11c7415351fccd5c982d173916988d6b82ae2507
Hunor Tot-Bagi <hunor.totbagi@rationaletech.com>
2026-01-23 21:34:43 +0100

    feat: finish quest6 p2

--- 2025/Test/Quest06Test.cs
+++ 2025/Test/Quest06Test.cs
@@ -7,6 +7,7 @@ namespace Test;
 public class Quest06Test
 {
     private readonly string _testData1 = AppDomain.CurrentDomain.BaseDirectory + "../../../../Src/Quest06/testData1.txt";
+    private readonly string _testData2 = AppDomain.CurrentDomain.BaseDirectory + "../../../../Src/Quest06/testData2.txt";
 
     [Fact]
     public void ReadFile()
@@ -22,13 +23,26 @@ public class Quest06Test
     }
 
     [Fact]
-    public void CountMentorsTest()
+    public void CountMentors1Test()
     {
         // Arrange
         var expected = 5;
 
         // Act
         var result = SolutionP1.CountMentors(_testData1);
+
+        // Assert
+        result.Should().Be(expected);
+    }
+
+    [Fact]
+    public void CountMentors2Test()
+    {
+        // Arrange
+        var expected = 11;
+
+        // Act
+        var result = SolutionP2.CountMentors(_testData2);
 
         // Assert
         result.Should().Be(expected);
