da4f3e3373bbb53fc236b2cae75f39836cc5534a
Hunor Tot-Bagi <hunortotbagi@gmail.com>
2026-03-06 21:01:58 +0100

    🎵💻 finish quest01 p1

---
+++ 2026/Src/Quest01/SolutionP1.cs
@@ -0,0 +1,61 @@
+﻿namespace Src.Quest01;
+
+public class SolutionP1
+{
+    public static double Calculate(string input)
+    {
+        var finalSumm = 0;
+        var (result, colors) = ReadData(input);
+
+        for (var i= 0; i<colors.Count; i++) 
+        {
+            var first = ConvertStringToNumber(colors[i][0]);
+            var second = ConvertStringToNumber(colors[i][1]);
+            var third = ConvertStringToNumber(colors[i][2]);
+
+            if (second > first && second > third)
+                finalSumm += result[i];
+        }
+
+        return finalSumm;
+    }
+
+    public static double ConvertStringToNumber(string input)
+    {
+        var length = input.Length;
+        double result = 0;
+        var power = 0;
+
+        for (var i = length - 1; i >= 0; i--)
+        {
+            if (char.IsUpper(input[i]))
+            {
+                result += Math.Pow(2, power);
+            }
+                
+            power++;
+        }
+
+
+        return result;
+    }
+
+
+    public static (List<int> results, List<string[]> colors) ReadData(string input)
+    {
+        var results = new List<int>();
+        var colors = new List<string[]>();
+
+        var lines = File.ReadAllLines(input);
+
+        foreach (var line in lines)
+        {
+            var parts = line.Split(":");
+
+            results.Add(int.Parse(parts[0]));
+            colors.Add(parts[1].Split(" "));
+        }
+
+        return (results, colors);
+    }
+}
---
+++ 2026/Test/Quest01Tests.cs
@@ -0,0 +1,59 @@
+﻿using FluentAssertions;
+using Src.Quest01;
+using Xunit;
+
+namespace Test;
+
+public class Quest01Tests
+{
+    private readonly string _testData1 = AppDomain.CurrentDomain.BaseDirectory + "../../../../Src/Quest01/testData1.txt";
+
+    [Fact]
+    public void ReadData()
+    {
+        // Arrange
+        var expectedResults = new List<int> { 2456, 7689, 3145, 6710 };
+        var expectedColors = new List<string[]>
+        {
+            new[] { "rrrrrr", "ggGgGG", "bbbbBB" },
+            new[] { "rrRrrr", "ggGggg", "bbbBBB" },
+            new[] { "rrRrRr", "gggGgg", "bbbbBB" },
+            new[] { "rrrRRr", "ggGGGg", "bbBBbB" }
+        };
+
+        // Act
+        var (results, colors) = SolutionP1.ReadData(_testData1);
+
+        // Assert
+        results.Should().BeEquivalentTo(expectedResults);
+        colors.Should().BeEquivalentTo(expectedColors);
+    }
+
+    [Theory]
+    [InlineData(11, "ggGgGG")]
+    [InlineData(0, "rrrrrr")]
+    [InlineData(7, "bbbBBB")]
+    [InlineData(6, "rrrRRr")]
+    [InlineData(55, "GGgGGG")]
+    public void ConvertStringToNumberTest(int expected, string input)
+    {
+        // Act
+        var result = SolutionP1.ConvertStringToNumber(input);
+
+        // Assert
+        result.Should().Be(expected);
+    }
+
+    [Fact]
+    public void CalculateTest()
+    {
+        // Arrange
+        var expected = 9166;
+
+        // Act
+        var result = SolutionP1.Calculate(_testData1);
+
+        // Assert
+        result.Should().Be(expected);
+    }
+}
