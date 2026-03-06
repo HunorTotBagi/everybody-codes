5da43c8e75dd325484df46be0611655baf48528b
Hunor Tot-Bagi <hunortotbagi@gmail.com>
2026-03-06 21:09:30 +0100

    feat: add `ReadFile` method and test (#2)
    Co-authored-by: Hunor Tot-Bagi <hunor.totbagi@rationaletech.com>

---
+++ 2025/Src/Quest07/SolutionP1.cs
@@ -0,0 +1,22 @@
+﻿namespace Src.Quest07;
+
+public class SolutionP1
+{
+    public static (string[] names, Dictionary<string,string[]> rules) ReadFile(string input)
+    {
+        var lines = File.ReadLines(input).ToList();
+
+        var names = lines[0].Split(',');
+
+        var rules = new Dictionary<string, string[]>();
+        for (var i = 2; i < lines.Count; i++)
+        {
+            var parts = lines[i].Split(" > ");
+            var key = parts[0];
+            var values = parts[1].Split(',');
+            rules[key] = values;
+        }
+
+        return (names, rules);
+    }
+}
